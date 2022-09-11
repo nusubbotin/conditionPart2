2385a6f2acc8e9509d64931a0a97ec05a489a52e
nusubbotin <nu.subbotin@ya.ru>
2022-09-11 20:32:58 +0300

    Выполнение требований по Задаче №5

--- src/Main.java
+++ src/Main.java
@@ -30,5 +30,53 @@ public class Main {
         } else {
             System.out.println(year + " год не является високосным");
         }
+
+        System.out.println("Задание 4:");
+        int deliveryDistance = 0;
+        double deliveryTime;
+        int deliveryTimeInt;
+
+        deliveryDistance = deliveryDistance - 20;
+
+        deliveryTime = (float) deliveryDistance / 40;
+        deliveryTime = deliveryTime + 1;
+
+        deliveryTimeInt = (int) deliveryTime;
+
+        if (deliveryTime > deliveryTimeInt) {
+            deliveryTimeInt++;
+        }
+
+        System.out.println("Потребуется дней: " + deliveryTimeInt);
+
+        System.out.println("Задание 5:");
+        short monthNumber = -1;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Введены не корректные входные данные! \n"+
+                        "Меся должен быть числом от 1 до 12.");
+        }
+
     }
 }
