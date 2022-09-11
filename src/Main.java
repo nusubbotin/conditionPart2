6654af4ab2df65610eccba8d45a48b5df64f6b22
nusubbotin <nu.subbotin@ya.ru>
2022-09-11 13:28:39 +0300

    Выполнение требований по Задаче №3

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,14 @@ public class Main {
         } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
+        System.out.println("Задание 3:");
+        int year = 2021;
+
+        if (year % 4 == 0 && year % 100 > 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
