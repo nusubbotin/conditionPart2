55f5afd4e85d9449cdece1dc7cfc3f30b24dcb62
nusubbotin <nu.subbotin@ya.ru>
2022-09-11 13:12:21 +0300

    Выполнение требований по Задаче №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание 1:");
+        int clientOS = 1;
 
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
