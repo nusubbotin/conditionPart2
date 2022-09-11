cec2888629e3b3a64049be86822ea3ffbb37458f
nusubbotin <nu.subbotin@ya.ru>
2022-09-11 13:19:02 +0300

    Выполнение требований по Задаче №2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,18 @@ public class Main {
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
+        System.out.println("Задание 2:");
+        int clientDeviceYear = 2015;
+
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
